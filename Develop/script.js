148f56ffd2853e292a10bf144ffeb8c2a3e6b594
Fausnightm <m.fausnight@lafilm.edu>
2021-06-12 16:20:31 -0400

    added number variable

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,9 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var uppercaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numberChar = [0,1,2,3,4,5,6,7,8,9];
 
 // Assignment code here
 
