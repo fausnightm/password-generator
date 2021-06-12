ae8edb01f6a686b005bde97c2beedabee6cbeebe
Fausnightm <m.fausnight@lafilm.edu>
2021-06-12 16:58:45 -0400

    added conditionals for characters

--- Develop/script.js
+++ Develop/script.js
@@ -23,4 +23,40 @@ function generatePass(){
      alert("It is recommended to have a password between 8 and 128 characters long!\nPlease start over.");
  }
 
+// Confirm the rest of character conditions
+ 
+ else{
+     if(confirm("Would you like your password to contain upper case letters?")){
+         Array.prototype.push.apply(allChars, upperCharArr);
+     }
+
+     if(confirm("Would you like your password to contain lower case letters?")){
+         Array.prototype.push.apply(allChars, lowerCharArr);
+     }
+
+     if(confirm("Would you like your password to contain numbers?")){
+         Array.prototype.push.apply(allChars, numArr);
+     }
+
+     if(confirm("Would you like your password to contain symbolbols?")){
+         Array.prototype.push.apply(allChars, symbolArr);
+     }
+
+     if(allChars.length===0){
+         alert("You must select at lease 1 type of characters to generate a password!\nPlease start over.");
+     }
+
+// Run for loop to use confirmed information and generate password as a result
+
+     else{
+         for(var i=0; i<Totlength; i++){
+             var random = Math.floor(Math.random()*allChars.length);
+             resultPass += allChars[random];
+         }
+     }
+     }
+
+// Display the result
+
+     document.getElementById("password").innerHTML = resultPass;
 }
